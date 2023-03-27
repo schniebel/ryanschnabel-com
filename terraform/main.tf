68112744f2e76b2821936a807ae9ad474df991b4
schniebel <ryan.schnabel@gmail.com>
2023-03-27 16:44:01 -0500

    adding provider versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+provider "null" {
+  version = "3.1.0"
+}
+
 resource "null_resource" "install_k3_master" {
   provisioner "remote-exec" {
     connection {
--- terraform/ryanschnabel-com-prod.tf
+++ terraform/ryanschnabel-com-prod.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
   config_context_cluster = "default"
+  version = "2.3.0"
 }
-
 
 resource "kubernetes_deployment" "welcome-nginx-prod" {
   metadata {
