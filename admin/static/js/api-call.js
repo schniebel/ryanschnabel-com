a9e42574d8b86a18c92a43e333eab4ee886325f8
Ryan Schnabel <48690772+schniebel@users.noreply.github.com>
2024-01-09 22:03:29 +0000

    adding js to button

---
+++ admin/static/js/api-call.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('callApiButton').addEventListener('click', function() {
+        document.getElementById('apiResponse').textContent = 'Yolo';
+    });
+});
