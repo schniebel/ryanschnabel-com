b83b78d0b2fa566c4ff1510c066db08a8339deb4
Ryan Schnabel <48690772+schniebel@users.noreply.github.com>
2024-01-09 22:31:42 +0000

    trying to hit api

--- admin/static/js/api-call.js
+++ admin/static/js/api-call.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('callApiButton').addEventListener('click', function() {
         var inputText = document.getElementById('inputText').value;
-        document.getElementById('apiResponse').textContent = 'Yolo ' + inputText;
+        
+        fetch('http://api-service.api.svc.cluster.local:8080/helloworld')
+            .then(response => response.text())
+            .then(data => {
+                var combinedResponse = data + ' ' + inputText;
+                document.getElementById('apiResponse').textContent = combinedResponse;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById('apiResponse').textContent = 'Error: ' + error.message;
+            });
     });
 });
